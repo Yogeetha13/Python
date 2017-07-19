d9da41c55b8d7ec2d212f52b09eef41506fb6b19
yogeetha susparsha <ys467@afsconnect1.njit.edu>
2017-07-19 16:28:30 -0400

    generates random number

---
+++ roll_dice.py
@@ -0,0 +1,15 @@
+import random
+Roll = random.randint(1, 6)
+if Roll == 1:
+    print("number is",Roll)
+    elif Roll == 2:
+         print("number is",Roll)
+    elif Roll == 3:
+	 print("number is",Roll)
+    elif Roll == 4:
+    	 print("number is",Roll)
+    elif Roll == 5:
+         print("number is",Roll)
+    else:
+         print("number is",Roll)
+
